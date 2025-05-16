d60f890dea40066a49cd7cb893d42d1318519cb5
Ricardo Chavez <gic.rc455@gmail.com>
2025-05-15 18:45:11 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    total_gramos = int(input("¿Cuantos gramos pesa la  pieza?: "))
+    resultado = total_gramos * 2.5 * 2
+    
+    print(f"El costo ideal de la pieza es: ${resultado}")    
+
+if __name__ == "__main__":
+    main()
+
