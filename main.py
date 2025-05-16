b70e4e16d3616a374ca4b85dcaf0d93eb3322f8b
Ricardo Chavez <gic.rc455@gmail.com>
2025-05-15 19:01:19 -0600

    function costo_filamento

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-def main():
+def costo_pieza():
     total_gramos = int(input("¿Cuantos gramos pesa la  pieza?: "))
     resultado = total_gramos * 2.5 * 2
     
-    print(f"El costo ideal de la pieza es: ${resultado}")    
+    print(f"El costo ideal de la pieza es: ${resultado}")
+
+def costo_filamento(filamento):
+    total = filamento / 1000
+    return print(f"El gramo de filamento va a costar: ${total}")
+
+def main():
+    filamento = int(input("Cuanto costo el filamento ($$$)?: "))
+    costo_filamento(filamento)
+    
 
 if __name__ == "__main__":
     main()
